20d145d60cb8974e99bfba90e322888b55ac71fd
Mzukisi Makaluza <mzukisimakaluza@gmail.com>
2019-01-30 12:58:36 +0200

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -14,13 +14,6 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 2500, createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount:1000, createdAt: 3433333}));
-store.dispatch(addExpense({ description: 'Water bill', amount: 2000, createdAt: 10004}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
